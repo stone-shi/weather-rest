c3b40ce05e844af725b5cbfe53406db2de52660b
stoneshi <stonexp@gmail.com>
2019-08-15 16:58:03 -0700

    added memory options

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 
 if [[ -z "${DEBUG_PORT}" ]]; then
   echo Run in normal model
-  java -Djava.security.egd=file:/dev/./urandom -jar /app.jar
+  java -Djava.security.egd=file:/dev/./urandom -jar $JAVA_OPTS /app.jar
 else
   echo "Run in debug mode on port ${DEBUG_PORT}"
-  java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=*:${DEBUG_PORT},suspend=n -jar /app.jar
+  java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=*:${DEBUG_PORT},suspend=n $JAVA_OPTS -jar /app.jar
 fi
